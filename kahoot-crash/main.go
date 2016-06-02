acb9c8736cbb3bcd4f70dbb492cae6545ecb26e6
duvahacker <eliott.cronsjo@hotmail.se>
2016-06-02 18:16:11 +0200

    Update main.go

--- kahoot-crash/main.go
+++ kahoot-crash/main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Fprintln(os.Stderr, "Usage: crash <game pin> <nickname>")
+		fmt.Fprintln(os.Stderr, "Usage: crash <120536> <fuckof>")
 		os.Exit(1)
 	}
 	gamePin, err := strconv.Atoi(os.Args[1])
